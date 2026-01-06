2e6f6912fe2a1dc253925e3f1c5e58e5b39f1f47
NickBauru-881 <nicolasantunes.mkt@gmail.com>
2026-01-06 15:41:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const premios = [
  "Bônus / Desconto de R$5mil",
  "Bônus / Desconto de R$3mil",
  "Bônus / Desconto de R$2mil",
- "Documento + IPVA Grátis",
  "1 ano de combustível grátis (1 abastecimento a álcool/mês)",
  "Até a tabela FIPE no seu Seminovo"
 ];
